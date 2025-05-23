95c3c60703ba9e2afe4ca7e906bcbb90c7d48092
Vivek Hipparkar <vivekhipparkar@gmail.com>
2025-05-23 12:11:25 +0530

    Initial commit for LeetCode Friends backend

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
